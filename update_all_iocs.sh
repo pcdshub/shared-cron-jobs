b8733872c7da8219e6e56781c8bbd6375c8b332f
Ken Lauer <klauer@slac.stanford.edu>
2022-02-16 11:40:27 -0800

    MNT: actually update it, use own token

--- update_all_iocs.sh
+++ update_all_iocs.sh
@@ -9,5 +9,11 @@ cd /cds/data/iocData/.all_iocs/ || exit 1
 git add iocs.json iocs.txt
 (git commit -am "Update IOC list $(date)" || true) > /dev/null
 
-source /reg/g/pcds/pyps/conda/.tokens/typhos.sh
-# /reg/g/pcds/pyps/conda/py39/envs/pcds-5.2.1/bin/python
+# source /reg/g/pcds/pyps/conda/.tokens/typhos.sh
+source /cds/home/k/klauer/Repos/typhos/confluence.sh
+
+cd /cds/home/k/klauer/Repos/cron
+./confluence_page_from_json.py \
+    /cds/data/iocData/.all_iocs/iocs.json \
+    'PCDS/Existing IOCs' \
+    name alias host_port disable script binary dir config_file
