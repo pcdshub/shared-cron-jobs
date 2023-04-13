344a3bc4dafa6da98fcc01ff42b1adb396e91990
Ken Lauer <klauer@slac.stanford.edu>
2023-04-13 11:04:23 -0700

    Add update_plc_summary and crontab

---
+++ update_plc_summary.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source /reg/g/pcds/engineering_tools/latest-released/scripts/pcds_conda
+
+cd /cds/home/k/klauer/Repos/plc-summary/docs || exit 1
+
+set -xe
+
+make html
+git add source/*.rst
+git commit -am "Updated projects on $(date)"
+
+cd ../
+make gh-pages
+git push origin master gh-pages
+
+git checkout master
