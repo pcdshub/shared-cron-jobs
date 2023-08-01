69fe4f3d48626ed6a4f43c7fb436b05bcdc95b1e
Ken Lauer <klauer@slac.stanford.edu>
2023-08-01 09:50:28 -0700

    MNT: more verbose module updates

--- sync_epics_modules.sh
+++ sync_epics_modules.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+echo "** Updating EPICS modules **"
+
+echo "** Updating kerberos/afs tokens..."
+source $HOME/dotfiles/bin/tokens
+
+echo "** Using the PCDS conda env..."
+source /cds/group/pcds/engineering_tools/latest-released/scripts/pcds_conda
+
 cd /afs/slac/g/cd/swe/git/repos/package/epics/modules
 
+echo "** Working directory:"
+pwd
+
+echo "** Creating new repositories, if necessary:"
 ./create-new-slac-epics-repos.sh
+
+echo "** Synchronizing existing repositories:"
 ./push2slac-epics-via-ssh.sh
+
+echo "Done."
