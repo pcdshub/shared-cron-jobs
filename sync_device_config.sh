3e7e1ea6da7332147f55c85f818c9a6051af51ca
Ken Lauer <klauer@slac.stanford.edu>
2023-08-15 08:00:54 -0700

    Automatically create PR

--- sync_device_config.sh
+++ sync_device_config.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+source /reg/g/pcds/engineering_tools/latest-released/scripts/pcds_conda
+
 device_config_directories=(
   /cds/group/pcds/pyps/apps/hutch-python/device_config
 )
+
+pr_title="Deploy branch status - $(date +"%B %Y")"
+pr_body="** This PR was created automatically ** This PR can be used to easily see when cron-based pushes to GitHub happen, and allow us an opportunity to review changes prior to merging into master."
 
 for path in ${device_config_directories[@]}; do
   echo -e "\nSynchronizing: $path"
@@ -11,5 +16,6 @@ for path in ${device_config_directories[@]}; do
   set -x
   git commit -am "Automatic backup @ $(date)"
   git push origin-ssh deploy || echo "Failed: git push failure '$path'"
+  gh pr create -B master -H deploy -b "$pr_body" -t "$pr_title" || echo "PR creation failed; may already exist"
   set +x
 done
