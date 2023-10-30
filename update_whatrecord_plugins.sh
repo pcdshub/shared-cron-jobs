54e508b181fc93467db417b4b129126cc77f1c54
Ken Lauer <klauer@slac.stanford.edu>
2023-10-30 10:03:13 -0700

    REF: release the whatrecord plugin info to pswww

--- update_whatrecord_plugins.sh
+++ update_whatrecord_plugins.sh
@@ -2,7 +2,13 @@
 
 cd /cds/home/k/klauer/Repos/cron || exit 1
 
-source pcds_conda
+LOG_FN="$HOME/Repos/cron/cron_${USER}_whatrecord_plugins.log"
+
+# Tee standard output to the log file:
+exec > "${LOG_FN}"
+# And redirect standard error to standard output:
+exec 2>&1
+
 cd ~/Repos/whatrecord
 ./run_dump_plugins_only.sh
-cp -f plugins-only.json.gz /cds/group/pcds/pswww/wrtest
+cp -f plugins-only.json.gz /cds/group/pcds/pswww/info
