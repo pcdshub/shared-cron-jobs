4703c9a8a85e76328c22fd1dde6c81ae56baf165
Zachary Lentz <zlentz@slac.stanford.edu>
2024-04-05 15:44:16 -0700

    ENH: move tcbsd sync to it's own script, include git add step

--- sync_tcbsd_config.sh
+++ sync_tcbsd_config.sh
@@ -3,20 +3,26 @@
 source /cds/group/pcds/engineering_tools/latest-released/scripts/pcds_conda
 source "${HOME}/.pcdshub.sh"
 
-device_config_directories=(
-  /cds/group/pcds/pyps/apps/hutch-python/device_config
+bsd_config_directories=(
+  /cds/group/pcds/tcbsd/twincat-bsd-ansible
+)
+add_directories=(
+  host_vars
 )
 
 pr_title="Deploy branch status - $(date +"%B %Y")"
 pr_body="** This PR was created automatically ** This PR can be used to easily see when cron-based pushes to GitHub happen, and allow us an opportunity to review changes prior to merging into master."
 
-for path in ${device_config_directories[@]}; do
+for path in "${bsd_config_directories[@]}"; do
   echo -e "\nSynchronizing: $path"
-  cd $path || (echo "Failed: Invalid path? '$path'" && continue)
+  cd "${path}" || (echo "Failed: Invalid path? '$path'" && continue)
   pwd
   set -x
+  for dir in "${add_directories[@]}"; do
+    git add "${dir}"/*
+  done
   git commit -am "Automatic backup @ $(date)"
-  git push origin-https deploy || echo "Failed: git push failure '$path'"
-  gh pr create -B master -H deploy -b "$pr_body" -t "$pr_title" || echo "PR creation failed; may already exist"
+  git push origin-https deploy || echo "Failed: git push failure '${path}'"
+  gh pr create -B master -H deploy -b "${pr_body}" -t "${pr_title}" || echo "PR creation failed; may already exist"
   set +x
 done
