0e08312931e25300d12576778fa5d5af0cc4be38
tangkong <tangkongrobert@gmail.com>
2024-05-08 11:34:51 -0700

    MNT: fix typos

--- sync_atef_config.sh
+++ sync_atef_config.sh
@@ -3,14 +3,14 @@
 source /cds/group/pcds/engineering_tools/latest-released/scripts/pcds_conda
 source "${HOME}/.pcdshub.sh"
 
-bsd_config_directories=(
+atef_config_directories=(
   /cds/group/pcds/pyps/apps/atef/config
 )
 
 pr_title="Deploy branch status - $(date +"%B %Y")"
 pr_body="** This PR was created automatically ** This PR can be used to easily see when cron-based pushes to GitHub happen, and allow us an opportunity to review changes prior to merging into master."
 
-for path in "${bsd_config_directories[@]}"; do
+for path in "${atef_config_directories[@]}"; do
   echo -e "\nSynchronizing: $path"
   cd "${path}" || (echo "Failed: Invalid path? '$path'" && continue)
   pwd
